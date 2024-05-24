d38cf288524adb43ee630ad3f0ed2cfab0642b06
Eemeli Martti <eemeli.martti@futurice.com>
2024-05-24 16:29:38 +0300

    Use list elements

--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -7,11 +7,13 @@ const RestaurantList = async () => {
   return (
     <div className="py-4 max-w-screen-xl md:m-auto">
       <h2 className="text-xl mb-4">Restaurants</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+      <ol className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
         {data.map((restaurant) => (
-          <RestaurantCard {...restaurant} key={restaurant.id} />
+          <li key={restaurant.id}>
+            <RestaurantCard {...restaurant} />
+          </li>
         ))}
-      </div>
+      </ol>
     </div>
   );
 };
