0ca7416d521a1fa441e79181ec3dce7c11520c5d
Eemeli Martti <eemeli.martti@futurice.com>
2024-05-24 11:31:42 +0300

    Return mock restaurants instead of fetching

--- src/services/restaurants.ts
+++ src/services/restaurants.ts
@@ -1,12 +1,8 @@
+import 'server-only';
+
+import mockRestaurants from '@/app/mocks/restaurants';
 import { Restaurants } from '@/types/Restaurant';
 
 export const getRestaurants = async () => {
-  const res = await fetch('http://localhost:3000/api/restaurants');
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch restaurants');
-  }
-
-  const data = await res.json();
-  return Restaurants.parseAsync(data);
+  return Restaurants.parseAsync(mockRestaurants);
 };
