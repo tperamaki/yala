41c23786af5bf7efbd5f51ec29076a6fef59a58b
Eemeli Martti <eemeli.martti@futurice.com>
2024-05-24 16:19:26 +0300

    Add placeholder image

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -1,4 +1,5 @@
 import type { RestaurantT } from '@/types/Restaurant';
+import Image from 'next/image';
 
 const RestaurantCard = ({
   averageRating,
@@ -8,6 +9,12 @@ const RestaurantCard = ({
   return (
     <div className="flex bg-white mb-4 p-6 rounded-lg shadow-md">
       <div className="flex flex-col">
+        <Image
+          className="object-cover rounded-t-lg"
+          src="/placeholder.jpg"
+          alt="placeholder"
+          fill
+        />
         <h2 className="text-lg">{name}</h2>
         <span className="text-xs">{averageRating}</span>
       </div>
