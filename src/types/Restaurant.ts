b5a140c2ef81c6d9ed596f9c0092ffc1acc47403
Eemeli Martti <eemeli.martti@futurice.com>
2024-05-24 16:18:14 +0300

    Update Restaurant schema

--- src/types/Restaurant.ts
+++ src/types/Restaurant.ts
@@ -3,15 +3,15 @@ import { z } from 'zod';
 export const Restaurant = z.object({
   id: z.string().uuid(),
   name: z.string(),
-  category: z.string(),
-  description: z.string(),
-  address: z.string(),
+  categories: z.array(z.string()),
+  description: z.string().optional(),
+  address: z.string().optional(),
   createdAt: z.string(),
-  modifiedAt: z.string().date().or(z.null()),
+  modifiedAt: z.string().date().optional(),
   createdBy: z.string(),
-  modifiedBy: z.string().date().or(z.null()),
-  averageRating: z.number(),
-  ratings: z.number(),
+  modifiedBy: z.string().date().optional(),
+  averageRating: z.number().min(1).max(5),
+  amountOfReviews: z.number(),
 });
 
 export const Restaurants = z.array(Restaurant);
