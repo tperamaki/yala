77ba8946089992e6cc878a056065663a41a40259
Eemeli Martti <eemeli.martti@futurice.com>
2024-05-24 16:19:15 +0300

    Use RestaurantT as component prop type for now

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -1,9 +1,10 @@
-type RestaurantCardProps = {
-  averageRating: number;
-  name: string;
-};
+import type { RestaurantT } from '@/types/Restaurant';
 
-const RestaurantCard = ({ averageRating, name }: RestaurantCardProps) => {
+const RestaurantCard = ({
+  averageRating,
+  amountOfReviews,
+  name,
+}: RestaurantT) => {
   return (
     <div className="flex bg-white mb-4 p-6 rounded-lg shadow-md">
       <div className="flex flex-col">
