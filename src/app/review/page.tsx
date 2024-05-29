7ba9a7e30888d2996e3541bae03e7a86baf8eec0
Teemu Perämäki <teemu.peramaki@icloud.com>
2024-05-29 13:17:46 +0300

    Use tailwind prettier extensions to order classnames

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -3,28 +3,28 @@ import { addReview } from '@/services/review';
 const ReviewPage = () => {
   return (
     <main className="flex min-h-screen flex-col">
-      <h2 className="text-xl text-center">Add review</h2>
+      <h2 className="text-center text-xl">Add review</h2>
       <form
         action={addReview}
-        className="flex flex-col p-4 gap-2 justify-center items-center"
+        className="flex flex-col items-center justify-center gap-2 p-4"
       >
         <label htmlFor="restaurant">Restaurant</label>
         <input
           type="text"
           id="restaurant"
           name="restaurant"
-          className="max-w-md dark:text-slate-800 p-2 rounded-md border border-gray-300 dark:border-slate-700 focus:ring-2 focus:ring-gray-200 dark:focus:ring-slate"
+          className="dark:focus:ring-slate max-w-md rounded-md border border-gray-300 p-2 focus:ring-2 focus:ring-gray-200 dark:border-slate-700 dark:text-slate-800"
         />
         <label htmlFor="rating">Rating</label>
         <input
           type="number"
           id="rating"
           name="rating"
-          className="max-w-md dark:text-slate-800 p-2 rounded-md border border-gray-300 dark:border-slate-700 focus:ring-2 focus:ring-gray-200 dark:focus:ring-slate"
+          className="dark:focus:ring-slate max-w-md rounded-md border border-gray-300 p-2 focus:ring-2 focus:ring-gray-200 dark:border-slate-700 dark:text-slate-800"
         />
         <button
           type="submit"
-          className="p-2 bg-red-100 dark:bg-red-900 dark:text-white rounded-md max-w-max"
+          className="max-w-max rounded-md bg-red-100 p-2 dark:bg-red-900 dark:text-white"
         >
           Submit
         </button>
--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -5,9 +5,9 @@ const RestaurantList = async () => {
   const data = await getRestaurants();
 
   return (
-    <div className="py-4 max-w-screen-xl md:m-auto">
-      <h2 className="text-xl mb-4">Restaurants</h2>
-      <ol className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+    <div className="max-w-screen-xl py-4 md:m-auto">
+      <h2 className="mb-4 text-xl">Restaurants</h2>
+      <ol className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
         {data.map((restaurant) => (
           <li key={restaurant.id}>
             <RestaurantCard {...restaurant} />
