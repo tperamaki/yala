2def6cfdadc79bf1828a87b90e20c62a823fe559
Eemeli Martti <eemeli.martti@futurice.com>
2024-06-04 12:33:17 +0300

    Update review.ts
    - Remove redirect
    - Refactor returned values

--- src/services/review.ts
+++ src/services/review.ts
@@ -8,9 +8,14 @@ import {
   ReviewSchema,
 } from '@/types/generated';
 import { z } from 'zod';
-import { redirect } from 'next/navigation';
 
 const prisma = new PrismaClient();
+
+type AddReviewFormState = {
+  errors?: {
+    [key in keyof z.infer<typeof ReviewCreateInputSchema>]?: string[];
+  } & { send?: string[] };
+} & z.infer<typeof ReviewCreateInputSchema>;
 
 export const getReviews = async (restaurantId: number) => {
   const findManyArgs = ReviewFindManyArgsSchema.parse({
@@ -21,7 +26,10 @@ export const getReviews = async (restaurantId: number) => {
     .parseAsync(await prisma.review.findMany(findManyArgs));
 };
 
-export const addReview = async (formData: FormData) => {
+export const addReview = async (
+  prevState: AddReviewFormState,
+  formData: FormData,
+): Promise<AddReviewFormState> => {
   const payload = {
     restaurant: {
       connect: { id: parseInt(String(formData.get('restaurant')), 10) },
@@ -29,9 +37,21 @@ export const addReview = async (formData: FormData) => {
     rating: parseInt(String(formData.get('rating')), 10),
   };
 
-  const data = ReviewCreateInputSchema.parse(payload);
+  const validatedFields = await ReviewCreateInputSchema.safeParseAsync(payload);
 
-  await prisma.review.create({ data });
+  if (!validatedFields.success) {
+    return {
+      ...prevState,
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
 
-  redirect('/restaurants');
+  try {
+    await prisma.review.create({ data: validatedFields.data });
+    console.log('Created a new review');
+    return { ...validatedFields.data, errors: undefined };
+  } catch (error) {
+    console.error(error);
+    return { ...prevState, errors: { send: ['Failed to submit review'] } };
+  }
 };
