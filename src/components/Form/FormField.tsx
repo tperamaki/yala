0b1d8e25853eec30e181eefac0ca77703b39ba35
Eemeli Martti <eemeli.martti@futurice.com>
2024-06-04 12:43:25 +0300

    Render field error

--- src/components/Form/FormField.tsx
+++ src/components/Form/FormField.tsx
@@ -4,12 +4,14 @@ import Select from '../Select';
 import type { SelectProps } from '../Select';
 
 type FormRowProps = {
+  error?: string;
   label: string;
   name: string;
   children: React.ReactNode;
 };
 
 type FormFieldProps = {
+  error?: string;
   id: string;
   label: string;
   name: string;
@@ -25,18 +27,30 @@ type SelectFieldProps = {
 const inputClassNames =
   'appearance-none dark:focus:ring-slate rounded-md border border-gray-300 p-2 focus:ring-2 focus:ring-gray-200 dark:border-slate-700 dark:text-slate-800';
 
-const FormRow = ({ label, name, children }: FormRowProps) => {
+const FormRow = ({ error, label, name, children }: FormRowProps) => {
   return (
     <div className="flex flex-col gap-2">
       <label htmlFor={name}>{label}</label>
       {children}
+      {error && (
+        <p aria-live="polite" className="text-red-500">
+          {error}
+        </p>
+      )}
     </div>
   );
 };
 
-const FormField = ({ id, label, name, type, required }: FormFieldProps) => {
+const FormField = ({
+  error,
+  id,
+  label,
+  name,
+  type,
+  required,
+}: FormFieldProps) => {
   return (
-    <FormRow label={label} name={name}>
+    <FormRow error={error} label={label} name={name}>
       <input
         type={type}
         id={id}
@@ -52,10 +66,6 @@ export const NumberField = (props: Omit<FormFieldProps, 'type'>) => (
   <FormField {...props} type="number" />
 );
 
-export const TextField = (props: Omit<FormFieldProps, 'type'>) => (
-  <FormField {...props} type="text" />
-);
-
 export const SelectField = ({
   error,
   id,
@@ -66,4 +76,8 @@ export const SelectField = ({
   <FormRow error={error} label={label} name={name}>
     <Select id={id} name={name} className={inputClassNames} options={options} />
   </FormRow>
+);
+
+export const TextField = (props: Omit<FormFieldProps, 'type'>) => (
+  <FormField {...props} type="text" />
 );
