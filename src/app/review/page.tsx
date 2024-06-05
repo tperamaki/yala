4078ef121c5a5e620eee1d021780e958d9ec5357
Eemeli Martti <eemeli.martti@futurice.com>
2024-06-05 09:33:19 +0300

    Update export statement

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -1,9 +1,16 @@
-import AddReviewForm from '@/components/Form/AddReviewForm';
+import { AddReviewForm } from '@/components/Form';
 import { getRestaurants } from '@/services/restaurants';
 
 const ReviewPage = async () => {
   const restaurants = await getRestaurants();
-  const restaurantOptions = [{key: 'default', label: 'Please choose an option', value:""}, ...restaurants.map(restaurant => ({ key: restaurant.id, label: restaurant.name, value: restaurant.id }))]
+  const restaurantOptions = [
+    { key: 'default', label: 'Please choose an option', value: '' },
+    ...restaurants.map((restaurant) => ({
+      key: restaurant.id,
+      label: restaurant.name,
+      value: restaurant.id,
+    })),
+  ];
 
   return (
     <main className="flex min-h-screen flex-col">
--- src/components/Form/index.ts
+++ src/components/Form/index.ts
@@ -1,2 +1,4 @@
 export { default as Form } from './Form';
 export { NumberField, TextField, SelectField } from './FormField';
+export { default as AddRestaurantForm } from './AddRestaurantForm';
+export { default as AddReviewForm } from './AddReviewForm';
