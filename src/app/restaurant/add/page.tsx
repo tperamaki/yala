9439bd992a90af5d8a90fb9e0b382cf5d1fecc20
Eemeli Martti <eemeli.martti@futurice.com>
2024-06-05 09:34:16 +0300

    Use AddRestaurantForm

--- src/app/restaurant/add/page.tsx
+++ src/app/restaurant/add/page.tsx
@@ -1,16 +1,10 @@
-import { Form, TextField } from '@/components/Form';
+import { AddRestaurantForm, Form, TextField } from '@/components/Form';
 import { addRestaurant } from '@/services/restaurants';
 
 const AddRestaurantPage = () => {
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <Form
-        action={addRestaurant}
-        label="Add restaurant"
-        successMessage="Restaurant added!"
-      >
-        <TextField label="Name" id="name" name="name" required />
-      </Form>
+      <AddRestaurantForm />
     </main>
   );
 };
