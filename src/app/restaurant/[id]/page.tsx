6c148cd2ce3af66eb4ab3396dcdf5f6949e8af33
Teemu Perämäki <teemu.peramaki@icloud.com>
2024-07-01 10:48:09 +0300

    Add backarrow to review page

--- src/app/restaurant/[id]/page.tsx
+++ src/app/restaurant/[id]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import BackArrow from '@/components/BackArrow';
 import RestaurantCard from '@/components/RestaurantCard';
 import { useGetRestaurant } from '@/hooks/RestaurantHook';
 import Link from 'next/link';
@@ -9,25 +10,7 @@ const RestaurantsPage = (props: { params: { id: string } }) => {
   const { data, isLoading, isError } = useGetRestaurant(restaurantId);
   return (
     <main className="flex min-h-screen flex-col p-2">
-      <Link
-        href="/restaurant"
-        className="mb-4 text-black hover:text-red-500 dark:text-white"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="inline-block h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M15 19l-7-7 7-7"
-          />
-        </svg>
-      </Link>
+      <BackArrow href="/restaurant" />
       {isLoading && <p>Loading...</p>}
       {isError && <p>Something went wrong. Please try again.</p>}
       {!data && <p>Restaurant was not found.</p>}
