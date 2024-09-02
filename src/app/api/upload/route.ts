3bf0d83a002b34bf96dc19cdb399b25c1b384d55
Timo Harju <timo.harju@futurice.com>
2024-09-02 12:36:08 +0300

    Add support for uploading images to Azure Blob Storage
    - Implemented functionality to upload images to Azure Blob Storage instead of local disk storage.
    - Added environment variable `AZURE_IMAGE_STORAGE` to toggle between local and Azure storage.
    - If `AZURE_IMAGE_STORAGE` is set to `true`, images will be uploaded to the specified Azure Blob Storage container.
    - New environment variables `AZURE_IMAGE_STORAGE_CONNECTION_STRING`, `AZURE_IMAGE_STORAGE_HOST` and `AZURE_IMAGE_STORAGE_CONTAINER` are used to configure the Azure storage connection.

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,8 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { saveImage } from '@/services/image';
+import { saveImage, uploadImageToCloud } from '@/services/image';
 
 export const POST = async (request: NextRequest) => {
   try {
+    if (process.env.AZURE_IMAGE_STORAGE === 'true') {
+      return NextResponse.json(await uploadImageToCloud(request));
+    }
     return NextResponse.json(await saveImage(request));
   } catch (error) {
     console.error(error);
--- src/services/image.ts
+++ src/services/image.ts
@@ -2,6 +2,11 @@ import { NextRequest } from 'next/server';
 import { getSession } from '@auth0/nextjs-auth0';
 import fs from 'node:fs/promises';
 import { nanoid } from 'nanoid';
+import {
+  BlobServiceClient,
+  ContainerClient,
+  BlockBlobClient,
+} from '@azure/storage-blob';
 
 export const saveImage = async (request: NextRequest) => {
   const session = await getSession();
@@ -27,6 +32,58 @@ export const saveImage = async (request: NextRequest) => {
       url: `/uploads/img-${id}${extension}`,
     };
   }
+};
+
+export const uploadImageToCloud = async (request: NextRequest) => {
+  const session = await getSession();
+  if (!session?.idToken) {
+    throw new Error('Unauthorized');
+  }
+
+  const formData = await request.formData();
+  const body = Object.fromEntries(formData);
+  const file = (body.file as Blob) || null;
+
+  if (file !== null) {
+    const AZURE_IMAGE_STORAGE_CONNECTION_STRING =
+      process.env.AZURE_IMAGE_STORAGE_CONNECTION_STRING || '';
+    const AZURE_IMAGE_STORAGE_CONTAINER =
+      process.env.AZURE_IMAGE_STORAGE_CONTAINER || '';
+
+    const blobServiceClient = BlobServiceClient.fromConnectionString(
+      AZURE_IMAGE_STORAGE_CONNECTION_STRING,
+    );
+
+    const containerClient: ContainerClient =
+      blobServiceClient.getContainerClient(AZURE_IMAGE_STORAGE_CONTAINER);
+    const createContainerResponse = await containerClient.createIfNotExists();
+
+    const id = nanoid();
+    const filename = (body.file as File).name;
+    const extension = filename.slice(filename.lastIndexOf('.'));
+    const blobName = `img-${id}${extension}`;
+
+    const blockBlobClient: BlockBlobClient =
+      containerClient.getBlockBlobClient(blobName);
+
+    const arrayBuffer = await file.arrayBuffer();
+
+    const options = {
+      blobHTTPHeaders: { blobContentType: file.type },
+    };
+
+    const uploadBlobResponse = await blockBlobClient.upload(
+      arrayBuffer,
+      Buffer.byteLength(arrayBuffer),
+      options,
+    );
+
+    return {
+      success: true,
+      url: blockBlobClient.url,
+    };
+  }
+
   return {
     success: false,
     url: null,
